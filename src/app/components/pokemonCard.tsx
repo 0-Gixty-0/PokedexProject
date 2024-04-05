082b0b281d3c66d7d332a537df5b3f24f31878bf
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2024-04-05 18:59:46 +0200

    Create non-styled display of basic Pokémon info

--- src/app/components/pokemonCard.tsx
+++ src/app/components/pokemonCard.tsx
@@ -8,22 +8,47 @@ interface DisplayDataProps {
     url : string;
 }
 
-interface Ability {
-    ability: {
+// interface Ability {
+//     ability: {
+//         name: string;
+//         url: string;
+//     };
+// }
+//
+// interface Move {
+//     move: {
+//         name: string;
+//         url: string;
+//     };
+// }
+
+interface TypeEntry {
+    slot: number;
+    type: {
         name: string;
         url: string;
+    }
+}
+
+interface BasicPokemonEntry {
+    name: string;
+    base_experience: number;
+    height: number;
+    weight: number;
+    types: TypeEntry[];
+    sprites: {
+        other: {
+            "official-artwork": {
+                front_default: string;
+            };
+        };
     };
 }
 
-interface Move {
-    move: {
-        name: string;
-        url: string;
-    };
-}
+type DataEntry = null | BasicPokemonEntry;
 
 export default function PokemonCard(Props: DisplayDataProps) {
-    const [data, setData] = useState<any>(null)
+    const [data, setData] = useState<DataEntry>(null)
     const [error, setError] = useState<string | null>(null)
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
@@ -52,26 +77,31 @@ export default function PokemonCard(Props: DisplayDataProps) {
     if (error) return <p>This is the error: {error}</p>;
     if (!data) return null;
 
-    const abilitiesList = data.abilities.map((ability: Ability) =>
-        <li>
-            <PokemonAbilityCard
-                name={capitalizeFirstLetter(ability.ability.name)}
-                url={ability.ability.url}>
-            </PokemonAbilityCard>
-        </li>)
+    // const abilitiesList = data.abilities.map((ability: Ability) =>
+    //     <li>
+    //         <PokemonAbilityCard
+    //             name={capitalizeFirstLetter(ability.ability.name)}
+    //             url={ability.ability.url}>
+    //         </PokemonAbilityCard>
+    //     </li>)
+    //
+    // const movesList = data.moves.map((move: Move) =>
+    //     <li>
+    //         <PokemonMoveCard url={move.move.url}></PokemonMoveCard>
+    //     </li>)
 
-    const movesList = data.moves.map((move: Move) =>
-        <li>
-            <PokemonMoveCard url={move.move.url}></PokemonMoveCard>
-        </li>)
+    const types = data.types.map((entry: TypeEntry) =>
+    <li><p>{capitalizeFirstLetter(entry.type.name)}</p></li>)
 
     return (
         <div>
-            <h2>Fetched Pokemon {capitalizeFirstLetter(data.name)}</h2>
-            <h3>Abilities:</h3>
-            <ul>{abilitiesList}</ul>
-            <h3>Moves:</h3>
-            <ul>{movesList}</ul>
+            <h2>Fetched Pokemon: {capitalizeFirstLetter(data.name)}</h2>
+            <img src={data.sprites.other["official-artwork"].front_default} alt={"official artwork"}/>
+            <p>Type:</p>
+            <ul>{types}</ul>
+            <p>Height: {data.height} dm</p>
+            <p>Weight: {data.weight} hg</p>
+            <p>Base experience yield: {data.base_experience}</p>
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
